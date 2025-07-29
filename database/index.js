0f724ff07293f3f9403a510b465ecb6b5054a506
chiomaezekwe <chummyankie@gmail.com>
2025-07-29 05:43:21 +0100

    Fixed the use SSL for PostgreSQL on Render issue encountered during deployment

--- database/index.js
+++ database/index.js
@@ -1,12 +1,49 @@
 const { Pool } = require("pg")
 require("dotenv").config()
+
+/* ***************
+ * Connection Pool
+ * Render.com (production) requires SSL too!
+ * This setup enables SSL in both environments
+ * *************** */
+let pool
+
+pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+})
+
+// Development version with query logging
+if (process.env.NODE_ENV === "development") {
+  module.exports = {
+    async query(text, params) {
+      try {
+        const res = await pool.query(text, params)
+        console.log("executed query", { text })
+        return res
+      } catch (error) {
+        console.error("error in query", { text })
+        throw error
+      }
+    },
+  }
+} else {
+  // Production – export the pool directly
+  module.exports = pool
+}
+
+
+/*const { Pool } = require("pg")
+require("dotenv").config()
 /* ***************
  * Connection Pool
  * SSL Object needed for local testing of app
  * But will cause problems in production environment
  * If - else will make determination which to use
  * *************** */
-let pool
+/*let pool
 if (process.env.NODE_ENV == "development") {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
@@ -34,4 +71,4 @@ module.exports = {
     connectionString: process.env.DATABASE_URL,
   })
   module.exports = pool
-}
+} */
